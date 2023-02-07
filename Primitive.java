3229c2dc26b9e458a2f2c51302cd8b2cc7906b1d
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:37:35 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Primitive
+{
+public static void main(String args[])
+{
+boolean val=false;
+Boolean a=Boolean.valueOf(val);
+System.out.println(a);
+}
+}
