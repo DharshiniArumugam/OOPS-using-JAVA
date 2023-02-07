23f481cc94978ed7fa85229f7e2b88aae6bf9ae7
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:36:11 +0530

    Add files via upload

---
+++ Itostr.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Itostr
+{
+public static void main(String args[])
+{
+int val=150;
+String a=Integer.toString(val);
+System.out.println(a);
+}
+}
