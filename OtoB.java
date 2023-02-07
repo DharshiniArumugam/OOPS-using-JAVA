2aa63996fd22557ab53e3c7b810d923f871e8a22
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:37:03 +0530

    Add files via upload

---
+++ OtoB.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class OtoB
+{
+public static void main(String args[])
+{
+String a="true";
+Boolean bool=Boolean.valueOf(a);
+System.out.println(bool);
+}
+}
