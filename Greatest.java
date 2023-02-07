ffe6ec8563d9b9004fbfee1a3665eb47ae089818
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:53:33 +0530

    Add files via upload

---
+++ Greatest.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Greatest
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=s.nextInt();
+if(a>b)
+{
+System.out.printf("%d",a);
+}
+else 
+{
+System.out.printf("%d",b);
+}
+}
+}
