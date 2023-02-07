ec16d904297c5153f5d125276d4f4867eb9e419f
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:48:06 +0530

    Add files via upload

---
+++ Celtofar.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Celtofar
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+float a=s.nextFloat();
+double b;
+b=(a*1.8)+32;
+System.out.print(b);
+}
+}
+
