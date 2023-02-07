72ed2307659c80230c7209101a19acff497345b6
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:40:05 +0530

    Add files via upload

---
+++ Equals.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class Equals
+{
+public static void main(String args[])
+{
+int a[]={1,2,3,4};
+int b[]={1,2,3,4};
+int c[]={1,2,4,3};
+System.out.println(Arrays.equals(a,b));
+System.out.println(Arrays.equals(a,c));
+}
+}
