f17a76d990bdd9e4b611367a317da120b7f54d6c
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:47:27 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Arrays;
+public class Array
+{
+public static void main(String args[])
+{
+int[] age={12,24,31,48,50};
+for(int a:age)
+{
+System.out.println(a);
+}
+}
+}
