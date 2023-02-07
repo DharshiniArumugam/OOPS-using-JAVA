e47c72380de37289bfcd42b4cf15577518c756fb
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:39:12 +0530

    Add files via upload

---
+++ BOOL.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class BOOL
+{
+public static void main(String args[])
+{
+boolean bool1=false;
+boolean bool2=true;
+String a=new Boolean(bool1).toString();
+String b=new Boolean(bool2).toString();
+System.out.println(a);
+System.out.println(b);
+}
+}
