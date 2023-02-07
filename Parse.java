42ae038a3e7d20a59bd93f8be7b11169b579a1c0
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:38:04 +0530

    Add files via upload

---
+++ Parse.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Parse
+{
+public static void main(String args[])
+{
+String a="false";
+boolean bool=Boolean.parseBoolean(a);
+System.out.println(bool);
+}
+}
