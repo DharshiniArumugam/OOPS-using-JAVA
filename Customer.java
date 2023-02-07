d658f6ddf93e5b3721067de7174b30614a2ae48d
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:33:05 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Customer
+{
+int Accno=2001;
+String Name="Sakthi";
+Float Bal=200.34f;
+public static void main(String args[])
+{
+Customer c1=new Customer();
+System.out.println(c1.Accno);
+System.out.println(c1.Name);
+System.out.println(c1.Bal);
+}
+}
