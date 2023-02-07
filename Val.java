4a104ac052c0b947cadfc0cdb60d9b40b590b2a8
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:38:30 +0530

    Add files via upload

---
+++ Val.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Val
+{
+public static void main(String args[])
+{
+int val=100;
+System.out.println("Integer: "+val);
+boolean bool=(val==100);
+System.out.println("converted to Bool: "+bool);
+}
+}
