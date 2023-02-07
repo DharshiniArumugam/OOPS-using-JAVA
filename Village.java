0ca7e17ccb0df0ba8636d531bc26e53eb7aa18ba
Dharshini <124652612+DharshiniArumugam@users.noreply.github.com>
2023-02-07 19:40:32 +0530

    Add files via upload

---
+++ Village.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+public class Village
+{
+public static void main(String args[])
+{
+int H;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number");
+int i=s.nextInt();
+for(i=1;i>=20;i++)
+{
+if(i==20)
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+char a=s.Line();
+if(a=charAt(H))
+{
+System.out.print("H");
+}
+else
+{
+System.out.print("B");
+}
+}
+}
